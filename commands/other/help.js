0e855d19593db7203164b7d7f88f2f56c27bb5e3
Mr.Bill_Hub <75666113+Bill716@users.noreply.github.com>
2022-06-07 20:24:36 +0200

    command update

--- commands/other/help.js
+++ commands/other/help.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
     var help = new Discord.MessageEmbed()
     .setAuthor('Help :')
     .setDescription(`Prefix : ${client.config.prefix}\nList of commands :`)
-    .addField('NSFW  :underage: :', '`4k`, `futa`, `gonewild`, `blowjob`, `hthigh`, `feet`, `hyuri`, `lewdneko`, `paizuri`, `thigh`, `anal`, `ass`, `boobs`, `kanna`, `hanal`, `hass`, `hboobs`, `hentai`, `hkitsune`, `hmidriff`, `hneko`, `holo`, `kemonomimi`, `neko`, `pgif`, `pussy`, `yaoi`')
+    .addField('NSFW  :underage: :', '`4k`, `futa`, `gonewild`, `blowjob`, `hthigh`, `feet`, `hyuri`, `lewdneko`, `paizuri`, `thigh`, `anal`, `ass`, `hentai_anal`, `boobs`, `kanna`, `hanal`, `hass`, `hboobs`, `hentai`, `hkitsune`, `hmidriff`, `hneko`, `holo`, `kemonomimi`, `neko`, `pgif`, `pussy`, `yaoi`')
     .addField('NSW :', '`food`, `coffee`')
     .addField('Other :', '`help`, `gah`, `stats`')
     .setTimestamp()
